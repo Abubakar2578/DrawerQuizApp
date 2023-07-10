53abe646a6ef1c5018e2e6e9b370d288aa2cc9ea
HP <mianabubaker007@gmail.com>
2023-07-11 02:16:53 +0500

    DbHelper done

---
+++ app/src/main/java/com/example/drawerquizapp/DbHelper.java
@@ -0,0 +1,9 @@
+package com.example.drawerquizapp;
+
+public class DbHelper {
+    // sqlite class code is present here
+
+    public  void creat(){
+        System.out.println("jdchbxcjl");
+    }
+}
